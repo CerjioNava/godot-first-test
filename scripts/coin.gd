5172a9c6714ce996b0233a0a6641d7f29f1c0a6f
Sergio Nava <snava@intelix.biz>
2026-02-10 15:04:52 -0400

    feat: added coin and effects

---
+++ scripts/coin.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("I am a coin!")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+# Called when another body enters the area.
+func _on_body_entered(body: Node2D) -> void:
+	print("+1 coin!")
+	queue_free()
