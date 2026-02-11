b959f41edb47083a021af171c42e6d16fefdb992
Sergio Nava <snava@intelix.biz>
2026-02-11 16:26:57 -0400

    feat: added animations, death and more

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,13 +1,23 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+var is_death_player = false
 
 # On body entedered signal callback function
 func _on_body_entered(body: Node2D) -> void:
-	print("Player has entered the killzone!")
-	timer.start()
+	# print("Player has entered the killzone!")
+	# body.get_node("CollisionShape2D").queue_free()
+	
+	# Check if body is the player
+	if body is CharacterBody2D and body.name == "Player":
+		is_death_player = await body.take_damage()
+		print(is_death_player)
+		if is_death_player:
+			Engine.time_scale = 0.5
+			timer.start()
 
 # On timer timeout signal callback function
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	print("Player has been killed!")
 	get_tree().reload_current_scene()
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,22 +4,78 @@ extends CharacterBody2D
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+var is_hurt = false
+var is_death = false
+var hit_points = 3
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	# Action handling
+	if is_hurt or is_death:
+		# if hurt, set velocity to zero
+		velocity.x = 0
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Handle jump
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		
+		# ----- MOVEMENT -----
+		# Get the input direction
+		var direction := Input.get_axis("move_left", "move_right")
+		
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		# ----- MOVEMENT -----
+
+		# ----- ANIMATIONS -----
+		# Flip
+		if direction > 0:
+			animated_sprite.flip_h = false
+		elif direction < 0:
+			animated_sprite.flip_h = true
+		
+		if is_on_floor():
+			if direction != 0:
+				animated_sprite.play("run")
+			else:
+				animated_sprite.play("idle")
+		else:
+			if velocity.y <= 0:
+				animated_sprite.play("jump")
+			else:
+				animated_sprite.play("fall")
+		# ----- ANIMATIONS -----
 
 	move_and_slide()
+
+# Take damage function
+func take_damage():
+	# If it's hurting already, don't take damage
+	if is_hurt:
+		pass
+	
+	# Hurt process, when it's finished, return to normal state
+	is_hurt = true
+	hit_points -= 1
+	
+	# Check if player dies
+	print(hit_points)
+	if hit_points > 0:
+		animated_sprite.play("hurt")
+		await animated_sprite.animation_finished
+	else:
+		animated_sprite.play("death")
+		is_death = true
+	
+	is_hurt = false
+	return is_death
+
+# print animation name when it changes
+#func _on_animated_sprite_2d_animation_changed() -> void:
+	#print("Animation changed")
